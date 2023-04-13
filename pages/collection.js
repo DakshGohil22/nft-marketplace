d3adce3649559a82c5c3aa85b8c2c2f227464862
DakshGohil22 <dakshrajsinhgohil22@gmail.com>
2023-04-13 08:53:39 +0530

    this is an nft marketplace

---
+++ pages/collection.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState, useContext } from "react";
+
+import Style from "../styles/collection.module.css";
+import images from '../img';
+import { Banner, CollectionProfile, NFTCardTwo } from "/collectionPage/collectionindex";
+// import { NFTCardTwo } from "@/collectionPage/collectionIndex";
+// '../..collectionPage/collectionindex.js';
+import { Slider, Brand } from '@/components/componentsindex';
+import Filter from "../components/Filter/Filter";
+
+//SmartContract
+import { NFTMarketplaceContext, currentAccount } from '@/Context/NFTMarketplaceContext';
+
+
+const collection = () => {
+
+  const { fetchNFTs } = useContext(NFTMarketplaceContext);
+  const [nfts, setNfts] = useState([]);
+  const [nftsCopy, setNftsCopy] = useState([]);
+
+  useEffect(() => {
+    if (currentAccount) {
+      fetchNFTs().then((item) => {
+        setNfts(item.reverse());
+        setNftsCopy(item);
+      });
+    }
+  }, []);
+  // const collectionArray=[
+  //   images.nft_image_1,
+  // images.nft_image_2,
+  //images.nft_image_3,
+  //images.nft_image_1,
+  // images.nft_image_2,
+  //  images.nft_image_3,
+  //images.nft_image_1,
+  //images.nft_image_2,
+  //]
+  return (
+    <div className={Style.collection}>
+
+      <Banner bannerImage={images.creatorbackground1} />
+      <CollectionProfile />
+      <Filter />
+      <NFTCardTwo NFTData={nfts} />
+      <Slider />
+      <Brand />
+    </div>
+  );
+};
+
+export default collection;
